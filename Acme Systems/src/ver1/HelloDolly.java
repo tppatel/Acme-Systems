e437dbaa9259986eb99dc8773da52161d139086e
tppatel <trun210101@gmail.com>
2019-08-21 16:44:49 -0400

    Commit Elcipse Push

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,7 +3,8 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Dolly From Eclipse");
+		System.out.println("Hello Dolly From Eclipse Again");
 
 	}
 
